b64d7491ddb9ad5bc259a7a03cc39c8e4e909e63
Mayank Jani <mayank4net@gmail.com>
2012-06-06 14:39:24 +0530

    courses & grade CRUD

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,40 @@
 class CoursesController < ApplicationController
+	def index
+		@courses = Course.all
+	end
+
+	def new
+		@course = Course.new
+	end
+
+	def create
+		@course = Course.new(params[:course])
+		
+		if @course.save
+			redirect_to @course, notice: 'Course was successfully created.'
+		end		
+	end
+
+	def edit
+		@course = Course.find(params[:id])		
+	end
+
+	def update
+		@course = Course.find(params[:id])
+		
+		if @course.update_attributes(params[:course])
+			redirect_to @course, notice: 'Course was successfully updated'
+		end
+	end
+
+	def destroy
+		@course = Course.find(params[:id])
+		@course.destroy
+
+		redirect_to courses_path
+	end
+
+	def show
+		@course = Course.find(params[:id])
+	end
 end
--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,2 +1,40 @@
 class GradesController < ApplicationController
+	def index
+		@grades = Grade.all
+	end
+
+	def new
+		@grade = Grade.new
+	end
+
+	def create
+		@grade = Grade.new(params[:grade])
+		
+		if @grade.save
+			redirect_to @grade, notice: 'Grade was successfully created.'
+		end		
+	end
+
+	def edit
+		@grade = Grade.find(params[:id])		
+	end
+
+	def update
+		@grade = Grade.find(params[:id])
+		
+		if @grade.update_attributes(params[:grade])
+			redirect_to @grade, notice: 'Grade was successfully updated'
+		end
+	end
+
+	def destroy
+		@grade = Grade.find(params[:id])
+		@grade.destroy
+
+		redirect_to grades_path
+	end
+
+	def show
+		@grade = Grade.find(params[:id])
+	end
 end
