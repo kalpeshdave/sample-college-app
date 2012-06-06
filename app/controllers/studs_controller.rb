f4d0e0277f0979ad8c59b7c1c6dcc6d0ecc15877
Mayank Jani <mayank4net@gmail.com>
2012-06-06 12:30:14 +0530

    student CRUD

--- app/controllers/studs_controller.rb
+++ app/controllers/studs_controller.rb
@@ -1,5 +1,40 @@
 class StudsController < ApplicationController
 	def index
-		@welcome = "Welcome To Student Management"
+		@studs = Stud.all
+	end
+
+	def new
+		@stud = Stud.new
+	end
+
+	def create
+		@stud = Stud.new(params[:stud])
+		
+		if @stud.save
+			redirect_to @stud, notice: 'Student was successfully created.'
+		end		
+	end
+
+	def edit
+		@stud = Stud.find(params[:id])		
+	end
+
+	def update
+		@stud = Stud.find(params[:id])
+		
+		if @stud.update_attributes(params[:stud])
+			redirect_to @stud, notice: 'Student was successfully updated'
+		end
+	end
+
+	def destroy
+		@stud = Stud.find(params[:id])
+		@stud.destroy
+
+		redirect_to studs_path
+	end
+
+	def show
+		@stud = Stud.find(params[:id])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Student::Application.routes.draw do
 	resources :studs
 	resources :courses
 	resources :grades
+
+	root :to => 'studs#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
